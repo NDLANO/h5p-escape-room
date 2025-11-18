0b190b5accad1a740491fe6c5d8286f8962f2cc3
Oliver Tacke <o.tacke@posteo.de>
2025-11-18 19:12:42 +0100

    Remove duplicate property

--- scripts/components/Main.js
+++ scripts/components/Main.js
@@ -31,7 +31,6 @@ export default class Main extends React.Component {
     this.sceneAudioPlayers = {};
 
     this.state = {
-      zoomPercentage: 0,
       threeSixty: null,
       showingTextDialog: false,
       currentText: null,
