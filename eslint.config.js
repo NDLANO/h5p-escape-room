f62bc5370341b9307a8fb640363e2302564fc78f
Oliver Tacke <o.tacke@posteo.de>
2025-11-19 21:45:19 +0100

    Fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,13 @@
 import eslintConfigNdlaH5P from 'eslint-config-ndla-h5p';
 import pluginReact from 'eslint-plugin-react';
-import pluginJsdoc from 'eslint-plugin-jsdoc';
 
 export default [
   eslintConfigNdlaH5P.configs['flat/recommended'],
   pluginReact.configs.flat.recommended,
-  pluginJsdoc.configs['flat/recommended'],
   {
-    rules: {
-      'jsdoc/valid-types': 0,
-    },
     settings: {
       react: {
         version: 'detect',
-      },
-      jsdoc: {
-        preferredTypes: {
-          Function: 'function',
-        },
       },
     },
   },
